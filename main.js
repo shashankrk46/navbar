18c62ba07922838d81da233beb2ed3220fa7c6a3
shashankrk46 <shashankrk46@gmail.com>
2020-02-22 14:38:38 +0530

    lines animation

---
+++ main.js
@@ -0,0 +1,15 @@
+const navSlide=()=>{
+    const burger=document.querySelector('.burger');
+    
+
+    burger.addEventListener('click',()=>{
+      
+      
+    // berger animation
+    burger.classList.toggle('toggle');
+    });
+
+    
+}
+
+navSlide();
